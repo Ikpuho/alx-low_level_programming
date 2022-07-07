0527ce1a42f4ce9f22cef1420fac19ccb8339cb8
Ikpuho <inyangabasiikpuho@gmail.com>
2022-07-06 21:59:30 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 -> prints natural numbers to 98
+ * @n: is a parameter
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+if (n > 0 && n <= 98)
+{
+_putchar(n);
+return (0);
+}
+}
